f99ab45c2fbd3097634808f3ba3b9bfa759a796f
MAYER-Ti <s.lutov@inbox.ru>
2025-04-17 23:10:36 +0300

    мат моделирование 3я работа, сделана программа

---
+++ math_modeling/lab_3/GenRandNumb/normaldistributiongenerator.cpp
@@ -0,0 +1,24 @@
+#include "normaldistributiongenerator.h"
+
+NormalDistributionGenerator::NormalDistributionGenerator() : gen(rd()), dis(0.0, 1.0), hasSpare(false) {}
+
+double NormalDistributionGenerator::generate(double mean, double sigma)
+{
+    if (hasSpare) {
+        hasSpare = false;
+        return spare * sigma + mean;
+    }
+
+    double u, v, s;
+    do {
+        u = dis(gen) * 2.0 - 1.0;
+        v = dis(gen) * 2.0 - 1.0;
+        s = u * u + v * v;
+    } while (s >= 1.0 || s == 0.0);
+
+    s = std::sqrt(-2.0 * std::log(s) / s);
+    spare = v * s;
+    hasSpare = true;
+
+    return u * s * sigma + mean;
+}
